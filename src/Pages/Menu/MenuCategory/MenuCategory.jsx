99265e96f888f044eac45305e926fc92ed6d365f
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-06-21 17:01:14 +0600

    Display Different Menu Category Items With Cover

--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,20 @@
+import Cover from "../../Shared/Cover/Cover";
+import Menuitem from "../../Shared/MenuItem/Menuitem";
 
 
-const MenuCategory = () => {
+const MenuCategory = ({items,title,img}) => {
     return (
-        <div>
-            
+        <div className="pt-7">
+               { title && <Cover img={img} title="OUR MENU"></Cover>}
+               <div className="grid md:grid-cols-2 gap-10 my-16">
+                {items.map(item => (
+                    <Menuitem
+                        key={item._id}
+                        item={item}
+                    />
+                ))}
+              
+            </div>
         </div>
     );
 };
--- src/Pages/Menu/OurMenu/OurMenu.jsx
+++ src/Pages/Menu/OurMenu/OurMenu.jsx
@@ -1,17 +1,45 @@
 import { Helmet} from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import coverimg from'../../../assets/menu/banner3.jpg'
+import dessertimg from'../../../assets/menu/dessert-bg.jpeg'
+import pizzaimg from'../../../assets/menu/pizza-bg.jpg'
+import saladimg from'../../../assets/menu/salad-bg.jpg'
+import soupimg from'../../../assets/menu/soup-bg.jpg'
+import useMenu from '../../../Hooks/useMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 
 
 const OurMenu = () => {
+    const [menu]=useMenu();
+    const desserts= menu.filter(item=>item.category==='dessert');
+    const pizzas= menu.filter(item=>item.category==='pizza');
+    const salads= menu.filter(item=>item.category==='salad');
+    const soups= menu.filter(item=>item.category==='soup');
+    const offereds= menu.filter(item=>item.category==='offered');
+
     return (
         <div>
             <Helmet>
                 <title>Testy_deliciouse|OUR_MENU</title>
             </Helmet>
           <Cover img={coverimg} title="OUR MENU"></Cover>
-    
-       
+          {/* main cover page */}
+          <SectionTitle 
+          subHeading='Donot Miss'
+          heading="TODAY'S OFFER"></SectionTitle>
+          {/*  offered menu items*/}
+          <MenuCategory items={offereds}></MenuCategory>
+       {/* desserts items */}
+    <MenuCategory items={desserts}  title="Desserts"  img={dessertimg}></MenuCategory>
+    {/* Pizzas */}
+
+    <MenuCategory  items={pizzas}  title="pizzas" img={pizzaimg} ></MenuCategory>
+    {/* Salads */}
+    <MenuCategory  items={salads} title='salads' img={saladimg }></MenuCategory>
+    {/* soups */}
+
+    <MenuCategory items={soups} title="soups"img={soupimg}></MenuCategory>
         </div>
     );
 };
