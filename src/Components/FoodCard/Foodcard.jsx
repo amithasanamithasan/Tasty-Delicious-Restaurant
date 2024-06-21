508bfa1f0a40f21ac5a4f5069c3e29b283557316
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-06-22 00:59:37 +0600

    Load Menu Data From Server

--- src/Components/FoodCard/Foodcard.jsx
+++ src/Components/FoodCard/Foodcard.jsx
@@ -6,12 +6,12 @@ const Foodcard = ({item}) => {
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
         <figure><img className="rounded-lg" src={image}alt="Shoes" /></figure>
         <p className=" absolute right-0 bg-black text-white text-2xl top-4 mx-4  "> PRICE:${price}</p>
-        <div className="card-body">
+        <div className="card-body flex flex-col items-center ">
           <h2 className="card-title">{name}</h2>
           <p>{recipe}</p>
          
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Add to cart</button>
+            <button className=" btn-warning btn btn-outline border-0 border-b-4 mt-4 justify-center">Add to cart</button>
           </div>
         </div>
       </div>
