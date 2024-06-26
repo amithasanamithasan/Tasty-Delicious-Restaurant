54b79b0339e0fb83e5a4b4e7f279e92a4b9fbfdb
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-06-26 15:57:05 +0600

    Verify Token And Axios Interceptor

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -5,6 +5,34 @@ const axiosSecure=axios.create({
 })
 
 const useAxiosSecure = () => {
+    //request  interceptor to add authorization header for every secure call to the api 
+    axiosSecure.interceptors.request.use(function (config) {
+        
+        const token= localStorage.getItem('access-token')
+
+        
+        console.log('request stopped by interceptors',token)
+        config.headers.authorization=`Bearer ${token}`
+
+        // Do something before request is sent
+        return config;
+      }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+      });
+
+     // Add a response interceptor 401 and 403 status
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  }, function (error) {
+    const status=error.response.status;
+    console.log('status error inthe interceptor',status)
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  });
  return axiosSecure;
 };
 
