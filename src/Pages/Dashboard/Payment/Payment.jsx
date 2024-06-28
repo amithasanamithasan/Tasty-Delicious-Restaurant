ec72136a3090e724ea5695ac38e90813fb7ee316
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-06-28 23:43:14 +0600

    Install Stripe, Stripe For React And Create Card Element

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,12 +1,18 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
 
 
 const Payment = () => {
+    // TODO :add publishable key
+    const stripePromise = loadStripe   ('');
     return (
         <div>
             <SectionTitle heading="payment" subHeading="Please pay to eate"></SectionTitle>
             <div>
-                <h1 className="text-2xl">amit payment</h1>
+            <Elements stripe={stripePromise} >
+           
+            </Elements>
             </div>
         </div>
     );
